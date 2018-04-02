63675ac0d9568e718509ded176b8a4af72528e61
M Haidar Hanif <git@mhaidarhanif.com>
2018-04-02 15:49:50 +0700

    Listen for a click, then show the contacts

--- index.js
+++ index.js
@@ -1,10 +1,34 @@
+let addressBook = [];
+
+console.log("Address Book");
+
 const title = document.getElementById("title");
 const addButton = document.getElementById("add-button");
+const contacts = document.getElementById("contacts");
 
-const alertName = function() {
+const addContact = () => {
   const fullname = document.getElementById("full-name").value;
-  alert(fullname);
+  addressBook.push({ name: fullname });
+
+  console.log(fullname, "is added as a contact");
+  document.getElementById("full-name").value = "";
 };
 
-title.addEventListener("click", alertName);
-addButton.addEventListener("click", alertName);
+const showContacts = () => {
+  contacts.innerHTML = "";
+
+  addressBook.map(contact => {
+    const li = document.createElement("li");
+    const contactNode = document.createTextNode(contact.name);
+    li.appendChild(contactNode);
+
+    contacts.append(li);
+  });
+};
+
+const addContactButton = function() {
+  addContact();
+  showContacts();
+};
+
+addButton.addEventListener("click", addContactButton);
