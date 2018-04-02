1dfdffc2477b22a70825f967d4af47f0fc8d7790
M Haidar Hanif <git@mhaidarhanif.com>
2018-04-02 14:37:26 +0700

    Add onclick event

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
 console.log("Address Book");
 
+let addressBook = [];
+
+const contacts = document.getElementById("contacts");
+
 const addContact = () => {
-  console.log("add");
+  const fullname = document.getElementById("fullname").value;
+  addressBook.push({ name: fullname });
+
+  console.log(fullname, "is added as a contact");
+  document.getElementById("fullname").value = "";
+};
+
+const showContacts = () => {
+  contacts.innerHTML = "";
+
+  addressBook.map(contact => {
+    const li = document.createElement("li");
+    const contactNode = document.createTextNode(contact.name);
+    li.appendChild(contactNode);
+
+    contacts.append(li);
+  });
 };
