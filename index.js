ccb71e71eff14e636a415f2db4136ef59da6b4ba
M Haidar Hanif <git@mhaidarhanif.com>
2018-04-03 11:35:56 +0700

    Refactor to use storage so we can persist the data

--- index.js
+++ index.js
@@ -1,26 +1,54 @@
-let addressBook = [];
-
-console.log("Address Book");
-
 const title = document.getElementById("title");
 const addButton = document.getElementById("add-button");
 const contacts = document.getElementById("contacts");
 
-const addContact = () => {
+// -----------------------------------------------------------------------------
+
+const getContacts = () => {
+  if (localStorage.addressBook) {
+    const contactsArray = JSON.parse(localStorage.addressBook);
+    return contactsArray;
+  } else {
+    localStorage.setItem("addressBook", "[]");
+    return [];
+  }
+};
+
+// -----------------------------------------------------------------------------
+
+const setContacts = newContacts => {
+  localStorage.addressBook = JSON.stringify(newContacts);
+};
+
+// -----------------------------------------------------------------------------
+
+const addContact = newContact => {
+  const contactsArray = getContacts();
+  contactsArray.push(newContact);
+  setContacts(contactsArray);
+};
+
+// -----------------------------------------------------------------------------
+
+const submitContact = () => {
   const fullname = document.getElementById("full-name").value;
 
   if (fullname !== "") {
-    addressBook.push({ name: fullname });
-
-    console.log(fullname, "is added as a contact");
+    // push new contact to storage
+    addContact({ name: fullname });
+    // empty out the input box value
     document.getElementById("full-name").value = "";
   } else {
     alert("Input can't be empty");
   }
 };
 
+// -----------------------------------------------------------------------------
+
 const showContacts = () => {
   contacts.innerHTML = "";
+
+  const addressBook = getContacts();
 
   addressBook.map(contact => {
     const li = document.createElement("li");
@@ -31,9 +59,15 @@ const showContacts = () => {
   });
 };
 
+// -----------------------------------------------------------------------------
+
 const callAddThenShow = function() {
-  addContact();
+  submitContact();
   showContacts();
 };
 
+// -----------------------------------------------------------------------------
+
 addButton.addEventListener("click", callAddThenShow);
+
+showContacts();
